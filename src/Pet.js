e1cc2d39ed0df44243ebc7510efc813ff8be6566
hiten3 <hitz1993gmail.com>
2022-11-23 15:33:40 +0530

    changes

--- src/Pet.js
+++ src/Pet.js
@@ -9,13 +9,4 @@ const Pet = props => {
     
 }
 
-
-
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, props.name),
-//       React.createElement("h2", {}, props.animal),
-//       React.createElement("h2", {}, props.breed),
-//     ]);
-//   };
 export default Pet;
